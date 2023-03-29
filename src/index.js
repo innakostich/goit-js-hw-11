d36271cd1b6231efb8d04091ae607eaf5e0dd1a9
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-03-29 14:21:06 +0200

    adds js and sass files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import './sass/main.scss';
+// import { fetchImages } from './js/fetch-images';
+// import { renderGallery } from './js/render-gallery';
+// import { onScroll, onToTopBtn } from './js/scroll';
+// import Notiflix from 'notiflix';
+// import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
